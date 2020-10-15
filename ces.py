f64a948a8eb7f300e650375914da7672ce7779d5
nstvn <71223295+nstvn@users.noreply.github.com>
2020-10-15 17:15:50 +0500

    Добавлена проверка
    Проверка на ввод числа в шаг

--- ces.py
+++ ces.py
@@ -3,18 +3,35 @@ mas = []
 for i in a:
     mas.append(ord(i))
 
-k = int(input('Введи шаг: '))
+k = ''
+while type(k) != int():
+    try:
+        k = int(input('Введи шаг: '))
+        break
+    except ValueError:
+        print('Вы ввели не число')
+
 new_mas = []
 for i in mas:
-    if i >= 1040 and i <= 1071:
-        i += k%33
-        if i > 1071:
-            i = (i % 1071)+1040
+    if 65 <= i <= 90:
+        i += k
+        if i > 90:
+            i = (i % 90) + 65
         new_mas.append(i)
-    elif i >= 1072 and i <= 1103:
-        i += k %33
+    elif 97 <= i <= 122:
+        i += k
+        if i > 122:
+            i = (i % 122) + 97
+        new_mas.append(i)
+    elif 1040 <= i <= 1071:
+        i += k % 33
+        if i > 1071:
+            i = (i % 1071) + 1040
+        new_mas.append(i)
+    elif 1072 <= i <= 1103:
+        i += k % 33
         if i > 1103:
-            i = (i % 1103)+1072
+            i = (i % 1103) + 1072
         new_mas.append(i)
     else:
         new_mas.append(i)
